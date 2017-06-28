a458dd5e3ac23f12b15dac1c10f6219f51c353bf
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-06-27 20:40:00 -0400

    added wsgi file to main directory.

--- FMTC/settings.py
+++ FMTC/settings.py
@@ -97,6 +97,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'production': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'fmtc-db'),
     }
 }
 
