cef1857255d286635453a84dc7b21b64cdc3f09c
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-07-06 18:34:57 -0400

    added P{rocfile and changed settings in order to deploy on heroku.

--- FMTC/settings.py
+++ FMTC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,19 @@ STATIC_PRECOMPILER_ROOT = os.path.dirname(BASE_DIR + '/assets/')
 STATIC_ROOT = os.path.dirname(BASE_DIR + '/static/')
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR
+
+
+# ----- HEROKU -----
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
