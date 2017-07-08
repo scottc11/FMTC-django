8f689be043b6ab024ea7147e0af38a65ce936787
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-07-08 15:08:56 -0400

    bug2

--- FMTC/settings.py
+++ FMTC/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"), # static folder (not in any apps)
-    os.path.join(BASE_DIR, "media"),
-    # os.path.join(BASE_DIR, "static"),
+    'media',
+
 ]
 
 # Static files (CSS, JavaScript, Images)
